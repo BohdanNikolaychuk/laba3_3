22bf244dc29b0732f5f3e2974a63fb58cd2b32d9
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2020-05-06 21:07:35 +0300

    3

---
+++ oopLaba3_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace oopLaba3_3
+{
+    //підраховує кількість слів у текст
+    class Program
+    {
+        static void numsword(string str)
+        {
+          
+            string[] text = str.Split(new char[] { ' ',',',':',';','.'}, StringSplitOptions.RemoveEmptyEntries);
+           
+            Console.WriteLine("Кiлькiсть слiв="+text.Length);
+            using (var sw = new StreamWriter("D:\\output.txt", true))
+            {
+                sw.Write("Кiлькiсть слiв="+text.Length);
+               
+
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            string path = "D:\\input.txt";
+
+            using (var sr = new StreamReader(path))
+            {
+
+                string str = sr.ReadToEnd();
+                numsword(str);
+
+            }
+            
+        }
+    }
+}
